ceb86d832d63a484190e2315656f897978fec01b
redtad1 <rediat1b@gmail.com>
2023-07-30 01:21:42 -0500

    add link

--- javascript/index.js
+++ javascript/index.js
@@ -42,7 +42,8 @@ function updateCity(event) {
           <div class="time">${cityTime.format("h:mm:ss")}
           ${cityTime.format("A")}</small>
           </div>
-        </div>`;
+        </div>
+        <a href="/">All cities</a>`;
 }
 
 updateTime();
