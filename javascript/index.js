326f788a9e5728a5f8f875163696b5ec6d4cdbdd
redtad1 <rediat1b@gmail.com>
2023-07-30 01:14:32 -0500

    correct js

--- javascript/index.js
+++ javascript/index.js
@@ -26,12 +26,12 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
 
   citiesElement.innerHTML = `
   <div class="city">
