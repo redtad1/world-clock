102936b55703652a7cf6f31214d9aa91ba398d24
redtad1 <rediat1b@gmail.com>
2023-07-29 23:12:30 -0500

    javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  //LA
+  let angelElement = document.querySelector("#angel");
+  let angelDateElement = angelElement.querySelector(".date");
+  let angelTimeElement = angelElement.querySelector(".time");
+  let angelTime = moment().tz("America/Los_Angeles");
 
+  angelDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  angelTimeElement.innerHTML = angelTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //addis
+  let addisElement = document.querySelector("#addis");
+  let addisDateElement = addisElement.querySelector(".date");
+  let addisTimeElement = addisElement.querySelector(".time");
+  let addisTime = moment().tz("Africa/Addis_Ababa");
+
+  addisDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  addisTimeElement.innerHTML = addisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
